03d8a308bbf1d388787a6645d39e4da4be54ce70
Andrew Huang <andrew.huang12@gmail.com>
2017-11-09 13:43:07 -0800

    Fix source prop notation

--- Libraries/Sound/Sound.js
+++ Libraries/Sound/Sound.js
@@ -8,21 +8,21 @@
  *
  * @providesModule Sound
  */
-'use strict';
+"use strict"
 
-const LayoutAndTransformPropTypes = require('LayoutAndTransformPropTypes');
-const NativeMethodsMixin = require('NativeMethodsMixin');
-const PropTypes = require('prop-types');
-const React = require('React');
-const ReactNativeViewAttributes = require('ReactNativeViewAttributes');
-const UIManager = require('UIManager');
-const ReactNative = require('ReactNative');
-const StyleSheetPropType = require('StyleSheetPropType');
-const View = require('View');
+const LayoutAndTransformPropTypes = require("LayoutAndTransformPropTypes")
+const NativeMethodsMixin = require("NativeMethodsMixin")
+const PropTypes = require("prop-types")
+const React = require("React")
+const ReactNativeViewAttributes = require("ReactNativeViewAttributes")
+const UIManager = require("UIManager")
+const ReactNative = require("ReactNative")
+const StyleSheetPropType = require("StyleSheetPropType")
+const View = require("View")
 
-const createReactClass = require('create-react-class');
-const requireNativeComponent = require('requireNativeComponent');
-const resolveAssetSource = require('resolveAssetSource');
+const createReactClass = require("create-react-class")
+const requireNativeComponent = require("requireNativeComponent")
+const resolveAssetSource = require("resolveAssetSource")
 
 /**
  * Sound represents an audio source located within the scene. It supports
@@ -31,8 +31,8 @@ const resolveAssetSource = require('resolveAssetSource');
  *
  * Example usage:
  * ```
- * <Image style={{height: 2.0, width: 2.0}} source={uri: 'images/waterfall.jpg'}>
- *   <Sound source={uri: 'sounds/waterfall.wav'} />
+ * <Image style={{height: 2.0, width: 2.0}} source={{uri: 'images/waterfall.jpg'}}>
+ *   <Sound source={{uri: 'sounds/waterfall.wav'}} />
  * </Image>
  * ```
  *
@@ -116,13 +116,13 @@ const Sound = createReactClass({
      * whether the audio plays when the component is loaded.
      * This prop is scheduled to be deprecated. Use `playControl` instead.
      */
-    playStatus: PropTypes.oneOf(['play', 'pause', 'stop']),
+    playStatus: PropTypes.oneOf(["play", "pause", "stop"]),
 
     /**
      * Controls the playback status.  If not set, the value of `autoPlay` determines
      * whether the audio plays when the component is loaded.
      */
-    playControl: PropTypes.oneOf(['play', 'pause', 'stop']),
+    playControl: PropTypes.oneOf(["play", "pause", "stop"]),
 
     /**
      * playerState - playerState is a `MediaPlayerState` that controls video
@@ -143,15 +143,15 @@ const Sound = createReactClass({
      *
      * `volume={0.5}`
      */
-    volume: PropTypes.number,
+    volume: PropTypes.number
   },
 
   viewConfig: {
-    uiViewClassName: 'Sound',
+    uiViewClassName: "Sound",
     validAttributes: {
       ...ReactNativeViewAttributes.RCTView,
-      volume: true,
-    },
+      volume: true
+    }
   },
 
   getDefaultProps: function() {
@@ -160,27 +160,27 @@ const Sound = createReactClass({
       volume: 1.0,
       loop: false,
       playControl: null,
-      source: null,
-    };
+      source: null
+    }
   },
 
   getInitialState: function() {
     return {
       volume: 1.0,
-      muted: false,
-    };
+      muted: false
+    }
   },
 
   componentWillMount() {
     if (__DEV__) {
       if (this.props.playStatus) {
         console.warn(
-          'playStatus has been renamed to playControl. Please update your code before v2.0.0'
-        );
+          "playStatus has been renamed to playControl. Please update your code before v2.0.0"
+        )
       }
     }
     if (this.props.playerState) {
-      this._subscribe(this.props.playerState);
+      this._subscribe(this.props.playerState)
     }
   },
 
@@ -188,44 +188,44 @@ const Sound = createReactClass({
     if (__DEV__) {
       if (nextProps.playStatus) {
         console.warn(
-          'playStatus has been renamed to playControl. Please update your code before v2.0.0'
-        );
+          "playStatus has been renamed to playControl. Please update your code before v2.0.0"
+        )
       }
     }
     if (this.props.playerState !== nextProps.playerState) {
       if (this.props.playerState) {
-        this._unsubscribe(this.props.playerState);
+        this._unsubscribe(this.props.playerState)
       }
       if (nextProps.playerState) {
-        this._subscribe(nextProps.playerState);
+        this._subscribe(nextProps.playerState)
       }
     }
   },
 
   componentWillUnmount() {
     if (this.props.playerState) {
-      this._unsubscribe(this.props.playerState);
+      this._unsubscribe(this.props.playerState)
     }
   },
 
   _subscribe(playerState) {
-    playerState.addListener('play', this._play);
-    playerState.addListener('pause', this._pause);
-    playerState.addListener('seekTo', this._seekTo);
-    playerState.addListener('volumeChange', this._volumeChange);
-    playerState.addListener('mutedChange', this._mutedChange);
+    playerState.addListener("play", this._play)
+    playerState.addListener("pause", this._pause)
+    playerState.addListener("seekTo", this._seekTo)
+    playerState.addListener("volumeChange", this._volumeChange)
+    playerState.addListener("mutedChange", this._mutedChange)
     this.setState({
       volume: playerState.volume,
-      muted: playerState.muted,
-    });
+      muted: playerState.muted
+    })
   },
 
   _unsubscribe(playerState) {
-    playerState.removeListener('play', this._play);
-    playerState.removeListener('pause', this._pause);
-    playerState.removeListener('seekTo', this._seekTo);
-    playerState.removeListener('volumeChange', this._volumeChange);
-    playerState.removeListener('mutedChange', this._mutedChange);
+    playerState.removeListener("play", this._play)
+    playerState.removeListener("pause", this._pause)
+    playerState.removeListener("seekTo", this._seekTo)
+    playerState.removeListener("volumeChange", this._volumeChange)
+    playerState.removeListener("mutedChange", this._mutedChange)
   },
 
   _play() {
@@ -233,7 +233,7 @@ const Sound = createReactClass({
       ReactNative.findNodeHandle(this),
       UIManager.Sound.Commands.play,
       []
-    );
+    )
   },
 
   _pause() {
@@ -241,7 +241,7 @@ const Sound = createReactClass({
       ReactNative.findNodeHandle(this),
       UIManager.Sound.Commands.pause,
       []
-    );
+    )
   },
 
   _seekTo(timeSec) {
@@ -249,88 +249,90 @@ const Sound = createReactClass({
       ReactNative.findNodeHandle(this),
       UIManager.Sound.Commands.seekTo,
       [timeSec]
-    );
+    )
   },
 
   _volumeChange(volume) {
-    this.setState({volume: volume});
+    this.setState({ volume: volume })
   },
 
   _mutedChange(muted) {
-    this.setState({muted: muted});
+    this.setState({ muted: muted })
   },
 
   _onEnded: function() {
-    this.props.onEnded && this.props.onEnded();
+    this.props.onEnded && this.props.onEnded()
   },
 
   _onDurationChange: function(event) {
     if (this.props.playerState) {
-      this.props.playerState.onDurationChange(event);
+      this.props.playerState.onDurationChange(event)
     }
-    this.props.onDurationChange && this.props.onDurationChange(event);
+    this.props.onDurationChange && this.props.onDurationChange(event)
   },
 
   _onTimeUpdate: function(event) {
     if (this.props.playerState) {
-      this.props.playerState.onTimeUpdate(event);
+      this.props.playerState.onTimeUpdate(event)
     }
-    this.props.onTimeUpdate && this.props.onTimeUpdate(event);
+    this.props.onTimeUpdate && this.props.onTimeUpdate(event)
   },
 
   _onPlayStatusChange: function(event) {
     if (this.props.playerState) {
-      this.props.playerState.onPlayStatusChange(event);
+      this.props.playerState.onPlayStatusChange(event)
     }
-    this.props.onPlayStatusChange && this.props.onPlayStatusChange(event);
+    this.props.onPlayStatusChange && this.props.onPlayStatusChange(event)
   },
 
   render: function() {
-    const props = {...this.props} || {};
-    props.style = props.style || {};
+    const props = { ...this.props } || {}
+    props.style = props.style || {}
     if (__DEV__) {
       if (props.children) {
-        console.warn('<Sound> must be a leaf node, props.children will not be rendered');
+        console.warn(
+          "<Sound> must be a leaf node, props.children will not be rendered"
+        )
       }
     }
     if (props.playStatus && !props.playControl) {
-      props.playControl = props.playStatus;
-      delete props['playStatus'];
+      props.playControl = props.playStatus
+      delete props["playStatus"]
     }
     if (this.props.playerState) {
-      props.autoPlay = false;
-      props.volume = this.state.volume;
-      props.muted = this.state.muted;
+      props.autoPlay = false
+      props.volume = this.state.volume
+      props.muted = this.state.muted
     }
 
     // events
-    props.onDurationChange = this._onDurationChange;
-    props.onTimeUpdate = this._onTimeUpdate;
-    props.onPlayStatusChange = this._onPlayStatusChange;
+    props.onDurationChange = this._onDurationChange
+    props.onTimeUpdate = this._onTimeUpdate
+    props.onPlayStatusChange = this._onPlayStatusChange
 
-    const source = resolveAssetSource(props.source);
+    const source = resolveAssetSource(props.source)
 
     if (source) {
       // Default to 'absolute' position, and pass handle to native side.
       return (
         <RKSound
-          style={[{position: 'absolute'}, props.style]}
+          style={[{ position: "absolute" }, props.style]}
           {...props}
           onEnded={this._onEnded}
           testID={props.testID}
           onStartShouldSetResponder={() => true}
           onResponderTerminationRequest={() => false}
         />
-      );
+      )
     }
 
     // If no source property, render `null`
-    return null;
-  },
-});
+    return null
+  }
+})
 
-const RKSound = requireNativeComponent('Sound', Sound, {
-  nativeOnly: {},
-});
+const RKSound = requireNativeComponent("Sound", Sound, {
+  nativeOnly: {}
+})
 
-module.exports = Sound;
+module.exports = Sound
